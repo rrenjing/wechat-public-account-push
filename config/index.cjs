8705056dedb540d8cded5b11d7440af0a66b942a
rrenjing <renjing_njtech@163.com>
2025-10-24 21:05:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝','宝宝1'
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'okzCc2HXEj-qCDGHfxbKCOEZiVog','okzCc2MltIHj1jvIAN7ardro2REU'
+      id: 'okzCc2HXEj-qCDGHfxbKCOEZiVog','okzCc2EW3Z-bEpQNuN8CWfYyJFSo'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'S74Moi7tcaAWkB81R5IlgfZs8DEjMMuDbi9VvqVJwj0',
+      useTemplateId: 'S74Moi7tcaAWkB81R5IlgfZs8DEjMMuDbi9VvqVJwj0','S74Moi7tcaAWkB81R5IlgfZs8DEjMMuDbi9VvqVJwj0'
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-14',
       festivals: [
